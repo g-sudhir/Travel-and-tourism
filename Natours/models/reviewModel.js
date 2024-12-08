b538234f8b3c4acaa3e555bcaf11d582dafbe15b
g-sudhir <sudhir868198@gmail.com>
2024-12-08 19:24:09 +0530

    additional functional are added

--- Natours/models/reviewModel.js
+++ Natours/models/reviewModel.js
@@ -1,32 +1,49 @@
 const mongoose = require('mongoose');
 
-const reviewSchema = new mongoose.Schema({
+const reviewSchema = new mongoose.Schema(
+  {
     review: {
-        type: String,
-        required: [true, 'Review must contain a comment'],
-        minlength: [5, 'Review must have more or equal then 5 characters'],
-        maxlength: [500, 'Review must have less or equal then 500 characters']
+      type: String,
+      required: [true, 'Review must contain a comment'],
+      minlength: [5, 'Review must have more or equal then 5 characters'],
+      maxlength: [500, 'Review must have less or equal then 500 characters'],
     },
     rating: {
-        type: Number,
-        min: 1,
-        max: 5,
-        required: [true, 'Review must have a rating between 1 and 5']
+      type: Number,
+      min: 1,
+      max: 5,
+      required: [true, 'Review must have a rating between 1 and 5'],
     },
     createdAt: {
-        type: Date,
-        default: Date.now
+      type: Date,
+      default: Date.now,
     },
     tour: {
-        type: mongoose.Schema.ObjectId,
-        ref: 'Tour',
-        required: true
+      type: mongoose.Schema.ObjectId,
+      ref: 'Tour',
+      required: true,
     },
     user: {
-        type: mongoose.Schema.ObjectId,
-        ref: 'User',
-        required: true
-    }
+      type: mongoose.Schema.ObjectId,
+      ref: 'User',
+      required: true,
+    },
+  },
+  {
+    toJSON: { virtuals: true },
+    toObject: { virtuals: true },
+  }
+);
+
+reviewSchema.pre(/^find/, function (next) {
+  this.populate({
+    path: 'User',
+    select: 'name photo',
+  }).populate({
+    path: 'Tour',
+    select: 'name',
+  });
+  next();
 });
 
 const Review = mongoose.model('Review', reviewSchema);
