35fe5d3eb45c57eedd9dadd347aa0056261a5b46
g-sudhir <sudhir868198@gmail.com>
2024-12-12 09:10:54 +0530

    add this files

--- Natours/routes/toursRoutes.js
+++ Natours/routes/toursRoutes.js
@@ -3,7 +3,11 @@ const express = require('express');
 const tourController = require('./../controller/tourController');
 const router = express.Router();
 
-router.route('/top-5-cheap').get(tourController.aliasTopTours, tourController.getAllTours);
+router
+  .route('/top-5-cheap')
+  .get(tourController.aliasTopTours, tourController.getAllTours);
+
+router.route('/tours-within/:distance/center/:lating/unit/:unit');
 
 router
   .route('/')
